f323b48c6a5b2d3c10be94a590b394421481582b
Brian Anderson <andersrb@gmail.com>
2019-01-09 23:33:58 +0000

    wip jemalloc

--- src/server/service/debug.rs
+++ src/server/service/debug.rs
@@ -29,7 +29,7 @@ use raftstore::store::msg::Callback;
 use raftstore::store::Engines;
 use server::debug::{Debugger, Error};
 use server::transport::RaftStoreRouter;
-use util::{jemalloc, metrics, rocksdb_stats};
+use util::{metrics, rocksdb_stats};
 
 fn error_to_status(e: Error) -> RpcStatus {
     let (code, msg) = match e {
@@ -328,7 +328,7 @@ impl<T: RaftStoreRouter + 'static> debugpb_grpc::Debug for Service<T> {
                 let engines = debugger.get_engine();
                 resp.set_rocksdb_kv(box_try!(rocksdb_stats::dump(&engines.kv)));
                 resp.set_rocksdb_raft(box_try!(rocksdb_stats::dump(&engines.raft)));
-                resp.set_jemalloc(jemalloc::dump_stats());
+                resp.set_jemalloc(tikv_alloc::dump_stats());
             }
             Ok(resp)
         });
