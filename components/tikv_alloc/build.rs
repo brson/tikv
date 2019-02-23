31f1a95faf2b3d11d96141c962221e29a26afab0
Brian Anderson <andersrb@gmail.com>
2019-02-23 01:08:20 +0000

    wip

---
+++ components/tikv_alloc/build.rs
@@ -0,0 +1,17 @@
+use std::env;
+
+fn main() {
+    if env::var("CARGO_FEATURE_DEFAULT").is_ok() {
+        println!("default!");
+    }
+    if env::var("CARGO_FEATURE_JEMALLOC").is_ok() {
+        println!("jemalloc!");
+    }
+    if env::var("CARGO_FEATURE_JEMALLOCATOR").is_ok() {
+        println!("jemallocator!");
+    }
+    if env::var("CARGO_FEATURE_TCMALLOC").is_ok() {
+        println!("tcmalloc!");
+    }
+    //panic!();
+}
