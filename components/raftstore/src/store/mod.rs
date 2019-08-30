c57c260802adf7cde6654355d4a0c4ea79d2832f
Brian Anderson <andersrb@gmail.com>
2019-08-30 13:56:29 -0700

    raftstore: extract RAFT_INIT_LOG_TERM/INDEX
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- components/raftstore/src/store/mod.rs
+++ components/raftstore/src/store/mod.rs
@@ -2,6 +2,7 @@ pub mod fsm;
 
 pub mod local_metrics;
 pub mod metrics;
+pub mod peer_storage;
 pub mod snap;
 
 pub use self::snap::{Error as SnapError};
---
+++ components/raftstore/src/store/peer_storage.rs
@@ -0,0 +1,4 @@
+// When we create a region peer, we should initialize its log term/index > 0,
+// so that we can force the follower peer to sync the snapshot first.
+pub const RAFT_INIT_LOG_TERM: u64 = 5;
+pub const RAFT_INIT_LOG_INDEX: u64 = 5;
