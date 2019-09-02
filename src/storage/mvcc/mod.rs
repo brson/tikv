3d567cb15db9ec27db0ed6318c73a5e33f9a2836
Brian Anderson <andersrb@gmail.com>
2019-09-02 07:53:54 -0700

    storage: Give write its own error type
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- src/storage/mvcc/mod.rs
+++ src/storage/mvcc/mod.rs
@@ -13,7 +13,7 @@ pub use self::reader::EntryScanner;
 pub use self::reader::MvccReader;
 pub use self::reader::{Scanner, ScannerBuilder};
 pub use self::txn::{MvccTxn, MAX_TXN_WRITE_SIZE};
-pub use self::write::{Write, WriteType};
+pub use self::write::{Write, WriteType, Error as WriteError};
 
 use std::error;
 use std::io;
@@ -176,6 +176,16 @@ impl From<LockError> for Error {
             LockError::Io(e) => Error::Io(e),
             LockError::Codec(e) => Error::Codec(e),
             LockError::BadFormatLock => Error::BadFormatLock,
+        }
+    }
+}
+
+impl From<WriteError> for Error {
+    fn from(err: WriteError) -> Error {
+        match err {
+            WriteError::Io(e) => Error::Io(e),
+            WriteError::Codec(e) => Error::Codec(e),
+            WriteError::BadFormatWrite => Error::BadFormatWrite,
         }
     }
 }
