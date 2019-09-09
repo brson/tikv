35be746c907aa225b8e694bf5dbf866a842836a3
Brian Anderson <andersrb@gmail.com>
2019-09-09 21:52:53 +0000

    raftstore: extract peer_storage::INIT_EPOCH_*
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- src/raftstore/store/peer_storage.rs
+++ src/raftstore/store/peer_storage.rs
@@ -39,10 +39,8 @@ pub use raftstore2::store::peer_storage::RAFT_INIT_LOG_INDEX;
 const MAX_SNAP_TRY_CNT: usize = 5;
 const RAFT_LOG_MULTI_GET_CNT: u64 = 8;
 
-/// The initial region epoch version.
-pub const INIT_EPOCH_VER: u64 = 1;
-/// The initial region epoch conf_version.
-pub const INIT_EPOCH_CONF_VER: u64 = 1;
+pub use raftstore2::store::peer_storage::INIT_EPOCH_VER;
+pub use raftstore2::store::peer_storage::INIT_EPOCH_CONF_VER;
 
 // One extra slot for VecDeque internal usage.
 const MAX_CACHE_CAPACITY: usize = 1024 - 1;
