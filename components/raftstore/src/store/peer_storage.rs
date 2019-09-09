6026aaa6277c192f3c9de9c74bf891b93d3484e6
Brian Anderson <andersrb@gmail.com>
2019-09-09 21:52:53 +0000

    raftstore: extract peer_storage::INIT_EPOCH_*
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- components/raftstore/src/store/peer_storage.rs
+++ components/raftstore/src/store/peer_storage.rs
@@ -3,6 +3,11 @@
 pub const RAFT_INIT_LOG_TERM: u64 = 5;
 pub const RAFT_INIT_LOG_INDEX: u64 = 5;
 
+/// The initial region epoch version.
+pub const INIT_EPOCH_VER: u64 = 1;
+/// The initial region epoch conf_version.
+pub const INIT_EPOCH_CONF_VER: u64 = 1;
+
 pub const JOB_STATUS_PENDING: usize = 0;
 pub const JOB_STATUS_RUNNING: usize = 1;
 pub const JOB_STATUS_CANCELLING: usize = 2;
