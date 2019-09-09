687033cfe952e6e8316e039c2788b1f061e05920
Brian Anderson <andersrb@gmail.com>
2019-09-09 20:29:41 +0000

    raftstore: decouple store::msg from PeerFsm
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- src/raftstore/store/msg.rs
+++ src/raftstore/store/msg.rs
@@ -1,5 +1,6 @@
 // Copyright 2016 TiKV Project Authors. Licensed under Apache-2.0.
 
+use std::any::Any;
 use std::fmt;
 use std::time::Instant;
 
@@ -12,7 +13,6 @@ use kvproto::raft_serverpb::RaftMessage;
 use raft::SnapshotStatus;
 
 use crate::raftstore::store::fsm::apply::TaskRes as ApplyTaskRes;
-use crate::raftstore::store::fsm::PeerFsm;
 use crate::raftstore::store::util::KeysInfoFormatter;
 use crate::raftstore::store::SnapKey;
 use crate::storage::kv::CompactedEvent;
@@ -214,7 +214,7 @@ pub enum CasualMessage<K: KvEngine, R: KvEngine> {
 
     /// A test only message, it is useful when we want to access
     /// peer's internal state.
-    Test(Box<dyn FnOnce(&mut PeerFsm<K, R>) + Send + 'static>),
+    Test(Box<dyn FnOnce(&mut dyn Any) + Send + 'static>),
 }
 
 impl<K: KvEngine, R: KvEngine> fmt::Debug for CasualMessage<K, R> {
