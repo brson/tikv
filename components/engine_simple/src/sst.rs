d6bf80b6aee63c367784c04b93cd1e90b3912fe9
Brian Anderson <andersrb@gmail.com>
2021-07-05 21:15:28 -0400

    engine_simple: More SST
    Signed-off-by: Brian Anderson <andersrb@gmail.com>

--- components/engine_simple/src/sst.rs
+++ components/engine_simple/src/sst.rs
@@ -8,6 +8,7 @@ use crate::engine::SimpleEngine;
 use engine_traits::{
     CfName, ExternalSstFileInfo, IterOptions, Iterable, Iterator, Result, SeekKey,
     SstCompressionType, SstExt, SstReader, SstWriter, SstWriterBuilder,
+    Error,
 };
 
 use blocksy3::raw::fs_thread::FsThread;
@@ -67,19 +68,6 @@ pub struct SimpleSstReaderIterator {
     kv: Option<(Key, Value)>,
 }
 
-impl SimpleSstReaderIterator {
-    fn maybe_update_kv(&mut self) -> Result<()> {
-        if self.cursor.valid() {
-            let key = self.cursor.key();
-            let value = block_on(self.cursor.value()).engine_result()?;
-            self.kv = Some((key, value));
-        } else {
-            self.kv = None;
-        }
-        Ok(())
-    }
-}
-
 impl Iterator for SimpleSstReaderIterator {
     fn seek(&mut self, key: SeekKey) -> Result<bool> {
         match key {
@@ -88,19 +76,31 @@ impl Iterator for SimpleSstReaderIterator {
             SeekKey::Key(key) => self.cursor.seek_key(Key::from_slice(key)),
         }
 
-        self.maybe_update_kv()?;
-
-        Ok(self.cursor.valid())
+        self.update_kv()
     }
     fn seek_for_prev(&mut self, key: SeekKey) -> Result<bool> {
-        panic!()
+        match key {
+            SeekKey::Start => self.cursor.seek_first(),
+            SeekKey::End => self.cursor.seek_last(),
+            SeekKey::Key(key) => self.cursor.seek_key_rev(Key::from_slice(key)),
+        }
+
+        self.update_kv()
     }
 
     fn prev(&mut self) -> Result<bool> {
-        panic!()
+        if !self.valid()? {
+            return Err(blocksy3::anyhow!("prev on invalid iterator")).engine_result();
+        }
+        self.cursor.prev();
+        self.update_kv()
     }
     fn next(&mut self) -> Result<bool> {
-        panic!()
+        if !self.valid()? {
+            return Err(blocksy3::anyhow!("next on invalid iterator")).engine_result();
+        }
+        self.cursor.next();
+        self.update_kv()
     }
 
     fn key(&self) -> &[u8] {
@@ -115,11 +115,30 @@ impl Iterator for SimpleSstReaderIterator {
     }
 }
 
+impl SimpleSstReaderIterator {
+    fn update_kv(&mut self) -> Result<bool> {
+        block_on(async {
+            if self.cursor.valid() {
+                let key = self.cursor.key();
+                let value = self.cursor.value().await.engine_result()?;
+                self.kv = Some((key, value));
+                Ok(true)
+            } else {
+                self.kv = None;
+                Ok(false)
+            }
+        })
+    }
+}
+
 pub struct SimpleSstWriter {
     tree: Tree,
     batch_writer: BatchWriter,
     fs_thread: Arc<FsThread>,
     file_path: PathBuf,
+    smallest_key: Option<Vec<u8>>,
+    largest_key: Option<Vec<u8>>,
+    num_entries: u64,
 }
 
 impl SimpleSstWriter {
@@ -135,6 +154,9 @@ impl SimpleSstWriter {
 
         Ok(SimpleSstWriter {
             tree, batch_writer, fs_thread, file_path,
+            smallest_key: None,
+            largest_key: None,
+            num_entries: 0,
         })
     }
 }
@@ -144,22 +166,88 @@ impl SstWriter for SimpleSstWriter {
     type ExternalSstFileReader = SimpleExternalSstFileReader;
 
     fn put(&mut self, key: &[u8], val: &[u8]) -> Result<()> {
-        block_on(self.batch_writer.write(Key::from_slice(key), Value::from_slice(val))).engine_result()
+        if let Some(largest_key) = self.largest_key.as_ref() {
+            if key[..] <= largest_key[..] {
+                return Err(Error::Engine("Keys must be added in order".to_string()));
+            }
+        }
+
+        block_on(self.batch_writer.write(Key::from_slice(key), Value::from_slice(val))).engine_result()?;
+
+        self.num_entries += 1;
+
+        if let Some(curkey) = self.smallest_key.as_ref() {
+            if key[..] < curkey[..] {
+                self.smallest_key = Some(key.to_vec());
+            }
+        } else {
+            self.smallest_key = Some(key.to_vec());
+        }
+        if let Some(curkey) = self.largest_key.as_ref() {
+            if key[..] > curkey[..] {
+                self.largest_key = Some(key.to_vec());
+            }
+        } else {
+            self.largest_key = Some(key.to_vec());
+        }
+
+        Ok(())
     }
     fn delete(&mut self, key: &[u8]) -> Result<()> {
-        panic!()
+        if let Some(largest_key) = self.largest_key.as_ref() {
+            if key[..] <= largest_key[..] {
+                return Err(Error::Engine("Keys must be added in order".to_string()));
+            }
+        }
+
+        block_on(self.batch_writer.delete(Key::from_slice(key))).engine_result()?;
+
+        self.num_entries += 1;
+
+        if let Some(curkey) = self.smallest_key.as_ref() {
+            if key[..] < curkey[..] {
+                self.smallest_key = Some(key.to_vec());
+            }
+        } else {
+            self.smallest_key = Some(key.to_vec());
+        }
+        if let Some(curkey) = self.largest_key.as_ref() {
+            if key[..] > curkey[..] {
+                self.largest_key = Some(key.to_vec());
+            }
+        } else {
+            self.largest_key = Some(key.to_vec());
+        }
+
+        Ok(())
     }
     fn file_size(&mut self) -> u64 {
         panic!()
     }
     fn finish(self) -> Result<Self::ExternalSstFileInfo> {
+        if self.num_entries == 0 {
+            return Err(Error::Engine("can't create sst with no entries".to_string()));
+        }
+
         block_on(self.batch_writer.ready_commit(BatchCommit(0))).engine_result()?;
         self.batch_writer.commit_to_index(BatchCommit(0), Commit(0));
         block_on(self.batch_writer.close()).engine_result()?;
         block_on(self.tree.sync()).engine_result()?;
 
+        let path = self.file_path.clone();
+        let file_size = block_on(self.fs_thread.run(|ctx| -> blocksy3::anyhow::Result<u64> {
+            let path = path;
+            let file = ctx.open_append(&path)?;
+            let meta = file.metadata()?;
+            Ok(meta.len())
+        })).engine_result()?;
+
         Ok(SimpleExternalSstFileInfo {
             file_path: self.file_path.clone(),
+            smallest_key: self.smallest_key,
+            largest_key: self.largest_key,
+            num_entries: self.num_entries,
+            file_size,
         })
     }
     fn finish_read(self) -> Result<(Self::ExternalSstFileInfo, Self::ExternalSstFileReader)> {
@@ -196,6 +284,10 @@ impl SstWriterBuilder<SimpleEngine> for SimpleSstWriterBuilder {
 
 pub struct SimpleExternalSstFileInfo {
     file_path: PathBuf,
+    smallest_key: Option<Vec<u8>>,
+    largest_key: Option<Vec<u8>>,
+    num_entries: u64,
+    file_size: u64,
 }
 
 impl ExternalSstFileInfo for SimpleExternalSstFileInfo {
@@ -206,19 +298,19 @@ impl ExternalSstFileInfo for SimpleExternalSstFileInfo {
         self.file_path.clone()
     }
     fn smallest_key(&self) -> &[u8] {
-        panic!()
+        self.smallest_key.as_ref().unwrap()
     }
     fn largest_key(&self) -> &[u8] {
-        panic!()
+        self.largest_key.as_ref().unwrap()
     }
     fn sequence_number(&self) -> u64 {
         panic!()
     }
     fn file_size(&self) -> u64 {
-        panic!()
+        self.file_size
     }
     fn num_entries(&self) -> u64 {
-        panic!()
+        self.num_entries
     }
 }
 
